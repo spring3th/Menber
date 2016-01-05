21fb3d7913c08640ee48a6eb31e05892550f2dbb
spring3th <dengchangqing@gmail.com>
2016-01-05 13:41:14 +0800

    view add signup function

--- Manage/menber/views.py
+++ Manage/menber/views.py
@@ -75,3 +75,25 @@ def login(req):
             status = 'not_exist_or_passwd_err'
     content = {'active_menu':'homepage','status':status,'user':''}
     return render_to_response('login.html',content,context_instance=RequestContext(req))
+
+def setpasswd(req):
+    username = req.session.get('username','')
+    if username != '':
+        user = MyUser.objects.get(username=username)
+    else:
+        return HttpResponseRedirect('/login/')
+    status = ''
+    if req.POST:
+        post = req.POST
+        if user.user.check_password(post.get('old','')):
+            if post.get('new','') == post.get('new_re',''):
+                user.user.set_password(post.get('new',''))
+                user.user.save()
+                status = 'success'
+            else:
+                status = 're_err'
+        else:
+            status = 'passwd_err'
+    content = {'user':user,'active_menu':'homepage','status':status}
+    return render_to_response('setpasswd.html',content,context_instance=RequestContext(req))
+            
