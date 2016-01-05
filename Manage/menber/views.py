649db152ce0e09e6d3c065b60eb22f8a5ab5752c
spring3th <dengchangqing@gmail.com>
2016-01-05 11:39:59 +0800

    view add signup function

--- Manage/menber/views.py
+++ Manage/menber/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.template import loader,Context
-from django.http import HttpResponse
-from menber.models import Menbers
-from django.shortcuts import render_to_response
+from django.template import loader,Context,RequestContext
+from django.http import HttpResponse,HttpResponseRedirect
+from django import forms
+from menber.models import Menbers,MyUser
+from django.contrib import auth
+from django.shortcuts import render_to_response,render
+from django.contrib auth.models import User
 
 # Create your views here.
 
@@ -26,3 +28,26 @@ def search(request):
                                    {'menber':menber,'query':q})
     else:
         return render_to_response('search_form.html',{'error':True})
+
+def signup(req):
+    if req.session.get('username',''):
+        return HttpResponseRedirect('/')
+    status = ''
+    if  req.POST:
+        post = req.POST
+        passwd = post.get('passwd','')
+        repasswd = post.get('repasswd','')
+        if passwd != repasswd: 
+            status = 're_err'
+        else:
+            username = post.get('username','')
+            if User.objects.filter(username=username):
+                status = 'user_exist'
+            else:
+                newuser = User.objects.create_user(username=username,passwd=passwd,email=post.get('email',''))
+                newuser.save()
+                new_myuser = MyUser(user=newuser,nickname=post.get('nickname'),permission=1)
+                new_myuser.save()
+                status = 'success'
+    content = {'active_menu':'homepage','status':status,'user':''}
+    return render_to_response('signup.html',content,context_instance=RequestContext(req))
