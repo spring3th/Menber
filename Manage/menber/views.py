faf0afa3d22a794dd517195944ebea7ae560c97a
spring3th <dengchangqing@gmail.com>
2016-01-05 13:05:04 +0800

    view add signup function

--- Manage/menber/views.py
+++ Manage/menber/views.py
@@ -73,5 +73,5 @@ def login(req):
                 status = 'not_active'
         else:
             status = 'not_exist_or_passwd_err'
-    content = {'active_menu':'homepage','status':status.'user':''}
+    content = {'active_menu':'homepage','status':status,'user':''}
     return render_to_response('login.html',content,context_instance=RequestContext(req))
