72029cecf8fd5b12f23e55fe151dbe593ff6ba9b
spring3th <dengchangqing@gmail.com>
2016-01-05 11:21:02 +0800

    header add login singn

--- Manage/menber/models.py
+++ Manage/menber/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class MyUser(models.Model):
-	user = models.OneToOneField(User)
-	nickname = models.CharField(max_length = 16)
-	permission = models.IntegerField()
+    user = models.OneToOneField(User)
+    nickname = models.CharField(max_length = 16)
+    permission = models.IntegerField()
     def __str__(self):
         return self.user.username
     class Meta:
